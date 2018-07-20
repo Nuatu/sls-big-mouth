9d8b72cf3f3b0698a66ac0ac57d6a16dd6265ed9
Nuatu <Nuatu@users.noreply.github.com>
2018-07-19 17:02:53 -0700

    Added search-restaurants function and Cognito security for /restaurants/search endpoint

--- functions/get-index.js
+++ functions/get-index.js
@@ -9,6 +9,10 @@ const http = require('superagent-promise')(require('superagent'), Promise);
 const aws4 = require('aws4');
 const URL = require('url');
 
+const awsRegion = process.env.AWS_REGION;
+const cognitoUserPoolId = process.env.cognito_user_pool_id;
+const cognitoClientID =  process.env.cognito_client_id;
+
 // Reference to Env variable set in serverless.yml
 const restaurantsApiRoot = process.env.restaurants_api;
 const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
@@ -46,7 +50,15 @@ module.exports.handler = co.wrap(function*(event, context, callback) {
   let template = yield loadHtml();
   let restaurants = yield getRestaurants(); 
   let dayOfWeek = days[new Date().getDay()];
-  let html = Mustache.render(template, { dayOfWeek, restaurants }); 
+  let view = {
+    dayOfWeek,
+    restaurants,
+    awsRegion,
+    cognitoUserPoolId,
+    cognitoClientID,
+    searchUrl: `${restaurantsApiRoot}/search`
+  }
+  let html = Mustache.render(template, view);
 
   const response = {
     statusCode: 200,
